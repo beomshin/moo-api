c0d55dbfa8207f6a3467d4ba5c585b03bbeaf160
chzh859@naver.com <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>
2025-07-27 14:24:59 +0900

    # gradle task 생성 - mooAppBootJar 생성 - moo-app 구동 jar copy

--- moo-app/src/main/java/com/kr/moo/MooAppApplication.java
+++ moo-app/src/main/java/com/kr/moo/MooAppApplication.java
@@ -1,6 +1,5 @@
 package com.kr.moo;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -10,11 +9,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(basePackages = "com.kr.moo.persistence.repository")
 @EntityScan(basePackages = "com.kr.moo.persistence.entity")
 public class MooAppApplication {
-
-    @PostConstruct
-    public void init() {
-        SharedTestClass.run();
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(MooAppApplication.class, args);
