be8a78250a2938edd5c3905234ad067c0082fbb1
오가희 <ogh010@naver.com>
2025-07-27 11:44:12 +0900

    feat : 회원가입 post
    회원가입 엔드포인트 및 서비스 메서드 추가

--- moo-user-service/src/main/java/com/kr/moo/controller/UserController.java
+++ moo-user-service/src/main/java/com/kr/moo/controller/UserController.java
@@ -1,11 +1,10 @@
 package com.kr.moo.controller;
 
+import com.kr.moo.persistence.entity.UserEntity;
 import com.kr.moo.service.UserListService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,6 +19,17 @@ public class UserController {
     @GetMapping(value = "/dummy")
     public List<?> findDummy() {
         return userListService.findDummy();
+    }
+
+    /**
+     * 회원가입 컨트롤러
+     * @param params 회원정보
+     * @return pk
+     */
+    @PostMapping(value = "/save")
+    @ResponseBody
+    public Long join(@RequestBody UserEntity params){
+        return userListService.saveUser(params);
     }
 
 }
--- moo-user-service/src/main/java/com/kr/moo/service/UserListService.java
+++ moo-user-service/src/main/java/com/kr/moo/service/UserListService.java
@@ -1,8 +1,17 @@
 package com.kr.moo.service;
 
+import com.kr.moo.persistence.entity.UserEntity;
+
 import java.util.List;
 
 public interface UserListService {
 
     List<?> findDummy();
+
+    /**
+     * 회원가입 서비스
+     * @param params 회원정보
+     * @return pk
+     */
+    Long saveUser(UserEntity params);
 }
