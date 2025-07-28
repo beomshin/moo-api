211b2b5de5b92cbfd189a9eb294c9c27345f5ff2
오가희 <ogh010@naver.com>
2025-07-28 15:17:51 +0900

    feat : 등록일 setter

--- moo-core/src/main/java/com/kr/moo/persistence/entity/common/BaseEntity.java
+++ moo-core/src/main/java/com/kr/moo/persistence/entity/common/BaseEntity.java
@@ -3,10 +3,7 @@ package com.kr.moo.persistence.entity.common;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.DynamicInsert;
 
@@ -21,6 +18,7 @@ import java.sql.Timestamp;
 @DynamicInsert
 public class BaseEntity {
 
+    @Setter
     @Column(name = "created_at")
     @JsonFormat(shape= JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss")
     private Timestamp createdAt; // 등록일
