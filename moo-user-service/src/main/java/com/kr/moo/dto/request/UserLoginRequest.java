647774e146c512ff8db733240e04158852dcf2c7
오가희 <ogh010@naver.com>
2025-08-10 14:07:18 +0900

    feat : dto 유효성검사 추가 및 builder삭제

--- moo-user-service/src/main/java/com/kr/moo/dto/request/UserLoginRequest.java
+++ moo-user-service/src/main/java/com/kr/moo/dto/request/UserLoginRequest.java
@@ -9,18 +9,23 @@ import lombok.*;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class UserLoginRequest {
 
     /**
      * 로그인 request dto
      */
 
-    @NotBlank(message = "휴대폰 번호를 입력해주세요.")
+    @NotBlank (message = "휴대폰 번호를 입력해주세요.")
+    @Size (min = 10, message = "휴대폰 번호는 10자 이상 이어야합니다.")
     private String userTel;
 
-    @NotBlank(message = "비밀번호를 입력해주세요.")
+    @NotBlank (message = "비밀번호를 입력해주세요.")
+    @Pattern(
+            regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]).{8,}$",
+            message = "비밀번호는 영문, 숫자, 특수문자 포함 8자 이상이여야합니다."
+    )
     private String userPwd;
 
+    @NotNull
     private Long storeId; // 로그인할때 매장 아이디
 }
